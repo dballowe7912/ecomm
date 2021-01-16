2cc1e939eee5c0b6e8a9122e55abb8f45080737a
Donald Ballowe <dballowe7912@gmail.com>
2021-01-15 23:02:44 -0600

    Added sign in form component and sign-in-sign-up page

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import FormInput from '../form-input/form-input.component';
+
+import './sign-in.styles.scss';
+
+class SignIn extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    nandleSubmit = event => {
+        event.preventDefault();
+        
+        this.setState({ email: '', password: '' });
+    }
+
+    handleChange = event => {
+        console.log(event.target.value)
+
+        const { value, name } = event.target;
+        this.setState({
+            [name]: value
+        })
+    }
+
+    render() {
+        return(
+            <div className='sign-in'>
+                <h2>I already have an account</h2>
+                <span>Sign in with your email and password</span>
+
+                <form onSubmit={this.handleSubmit}>
+                    <FormInput
+                        name="email" 
+                        type="email"
+                        handleChange={this.handleChange}
+                        value={this.state.email} 
+                        label="email"
+                        required />
+                    <FormInput 
+                        name="password" 
+                        type="password" 
+                        handleChange={this.handleChange} 
+                        value={this.state.password} 
+                        label="password"
+                        required 
+                    />
+                    <input type="submit" value="Submit Form" />
+                </form>
+            </div>
+        )
+    }
+} 
+
+export default SignIn;
