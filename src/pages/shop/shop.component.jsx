fdb658a69aff9995fda875ce45fa67141a0c9873
Donald Ballowe <dballowe7912@gmail.com>
2021-01-17 10:34:04 -0600

    updated shop data

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,31 +1,28 @@
-import React, { Component } from 'react';
-import SHOP_DATA from './shop.data.jsx';
+import React from 'react';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component.jsx';
+import SHOP_DATA from './shop.data.js';
 
-class ShopPage extends Component {
-    constructor(props) {
-        super(props);
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
+class ShopPage extends React.Component {
+  constructor(props) {
+    super(props);
 
-    render() {
-        const { collections } = this.state;
-        return (
-            <div className='shop-page'>
-                {
-                    collections.map(({ id, ...otherCollectionProps }) => (
-                        <CollectionPreview key={id} { ...otherCollectionProps }/>
-                    ))
-                }
-            </div>
-        )
-    }
+    this.state = {
+      collections: SHOP_DATA
+    };
+  }
+
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className='shop-page'>
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default ShopPage;
-
-
