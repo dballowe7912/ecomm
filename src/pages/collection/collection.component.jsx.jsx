f12ad4c9dcf890c0ef6442f830f6712067dc04ba
Donald Ballowe <dballowe7912@gmail.com>
2021-01-19 21:13:20 -0600

    Updated collection page

--- src/pages/collection/collection.component.jsx.jsx
+++ src/pages/collection/collection.component.jsx.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import CollectionItem from '../../components/collection-item/collection-item.component';
+
+import { selectCollections } from '../../redux/shop/shop.selectors';
 
 import './collection.styles.scss';
 
-const CollectionPage = ({ match }) => (
+const CollectionPage = ({ collection }) => (
     <div className='collection-page'>
         <h2>Collection Page</h2>
     </div>
 )
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+    collection: selectCollections(ownProps.match.params.collectionId)
+})
+
+export default connect(mapStateToProps)(CollectionPage);
